3ce6256b2fc19ba4b448dae635444cfc5507e854
Gabriel Rios <127359852+Rios763@users.noreply.github.com>
2023-10-17 08:03:56 -0300

    Add files via upload

---
+++ JavaScript/scripts.js
@@ -0,0 +1,492 @@
+/*
+console.log('Hellow world');
+let nome = "Gabriel";
+
+console.log(nome);
+
+console.log(typeof 1.5);
+console.log(typeof 13);
+console.log(typeof -5);
+
+console.log(5+(2**4-3));
+
+console.log(3>5 || "teste" == "teste");
+
+console.log(false ? 5 : 4);
+
+
+ let vector = [11,21,23,433,50];
+ vector [0] =512; 
+
+ console.log(vector);
+
+ let vector2 = [11,21,23,433,50];
+ vector2 [-1] = 512; 
+ vector2 [-2] = 512; 
+
+ console.log(vector2);
+
+ console.log("5"*"3");
+
+ console.log("5"+"3");
+
+ console.log("dois" * "três");
+
+
+
+
+let ameixas = 5;
+console.log(ameixas * ameixas);
+console.log(`Tem ${ameixas} ameixas na cesta`);
+
+ameixas +=1;
+console.log(ameixas);
+
+let quadrado=4, triangulo=3,retangulo=4;
+console.log(quadrado*(triangulo+retangulo));
+
+let nomenclatura="Armando";
+console.log(`Meu nome e ${nomenclatura}`);
+let Nomenclatura="Josue";
+console.log(`Meu nome e ${Nomenclatura}`);
+
+let laranjas = 50
+console.log(laranjas);
+console.log(laranjas*3);
+laranjas = 10
+console.log(laranjas);
+console.log(laranjas*3);
+
+var coiso="uau";
+console.log(`Hm....${coiso}`);
+
+const noice="hmmm"
+console.log(`aaaaaaaaaaaaaaaaaaa ${noice}`);
+
+const sobrenome="silva";
+
+var funciona=1;
+
+/*
+let variavel=prompt("Fala ai fera, mete o nome ae\nvoce: ");
+console.log(`coe, ${variavel}, de boa?`);
+
+let numerinhofds=parseInt(prompt(`Fala ai sua idade, ${variavel}, na moral\nvoce:`));
+
+
+if (numerinhofds>=18) 
+    console.log("Já pode ser preso");
+else 
+    console.log("Muito kid ainda");
+
+
+console.log(`legal, ${numerinhofds} anos, ja bebeu?`);
+*/
+
+//alert("E agora, José?\nA festa acabou\nA luz apagou\nO povo sumiu\nA noite esfriou\nE agora, José?\nE agora, você?\nVocê que é sem nome\nQue zomba dos outros\nVocê que faz versos\nQue ama, protesta?\nE agora, José?\n\nEstá sem mulher\nEstá sem discurso\nEstá sem carinho\nJá não pode beber\nJá não pode fumar\nCuspir já não pode\nA noite esfriou\nO dia não veio\nO bonde não veio\nO riso não veio\nNão veio a utopia\nE tudo acabou\nE tudo fugiu\nE tudo mofou\nE agora, José?\n\nE agora, José?\nSua doce palavra\nSeu instante de febre\nSua gula e jejum\nSua biblioteca\nSua lavra de ouro\nSeu terno de vidro\nSua incoerência\nSeu ódio, e agora?\n\nCom a chave na mão\nQuer abrir a porta\nNão existe porta\nQuer morrer no mar\nMas o mar secou\nQuer ir para Minas\nMinas não há mais\nJosé, e agora?\n\nSe você gritasse\nSe você gemesse\nSe você tocasse\nA valsa vienense\nSe você dormisse\nSe você cansasse\nSe você morresse\nMas você não morre\nVocê é duro, José!\n\nSozinho no escuro\nQual bicho-do-mato\nSem teogonia\nSem parede nua\nPara se encostar\nSem cavalo preto\nQue fuja a galope\nVocê marcha, José!\nJosé, para onde?");
+/*
+let maximo = parseInt( Math.max(vector));
+console.log(maximo);
+
+
+let escolhas = Math.ceil(14.3);
+console.log(escolhas);
+escolhas = Math.floor(14.3);
+console.log(escolhas);
+
+escolhas = Math.pow(5,8);
+console.log(escolhas);
+
+valor = Math.abs(-536);
+console.log(valor);
+
+console.log('aqui');
+valor = Math.ceil(14.6);
+console.log(valor);
+valor = Math.ceil(14.2);
+console.log(valor);
+valor = Math.ceil(-14.6);
+console.log(valor);
+valor = Math.floor(100.25);
+console.log(valor);
+valor = Math.floor(-100.25);
+console.log(valor);
+valor = Math.pow(5,3);
+console.log(valor);
+
+console.log('\n nonono \n\n\n')
+
+valor = Math.sqrt(125);
+console.log(valor);
+valor = Math.cbrt(81);
+console.log(valor);
+
+console.log("wow pulei\n\t\twow to aqui\n\t\t\t\t\tpulei\n\n");
+
+ 
+
+/*
+if (0){
+    console.log("wow");
+}
+console.log("oh no");
+
+let idade = prompt(`Digite sua idade`);
+
+if(idade > 18){
+    console.log('Pode entrar')
+} else{
+    console.log('Não pode entrar kid!!!!');
+}
+*/
+
+
+/*
+let A = parseInt(prompt('digite valor de A'));
+let B = parseInt(prompt('digite valor de B'));
+
+if ( (A+B)==3){
+    console.log('O resultado é 3');
+} else if(A==4){
+    console.log('O valor de A é 4');
+} else if(B==3){
+    console.log('O valor de B é 3');
+} else {
+    console.log('Nenhuma das condições acima');
+}
+
+*/
+/**
+ * for (asa=0; asa<100; asa++) {
+    console.log(asa);
+    
+}
+ */
+
+
+/* funcao soma
+
+
+const soma = function (a, b){
+    return a+b;
+}
+
+
+console.log(soma(3,5));
+
+*/
+
+/*funcao de multiplicacao e saudacao
+
+
+const saudacao = function(nome){
+    if(nome == "Matheus"){
+        return "Ola Matheus";
+    }
+}
+
+console.log(saudacao("Matheus"));
+
+let y=5;
+
+const multiplicar=  function(n){
+    let y=n*2;
+    console.log(y);
+
+    if (y==10){
+        let y=55;
+        console.log(y);
+
+    }
+
+    
+}
+
+multiplicar(y);
+console.log(y);
+
+*/
+
+
+/*
+function nomeComIdade(nome,idade){
+    if (idade==undefined){
+        console.log("Seu nome é: "+nome);
+
+    }else {
+        console.log("Seu nome é: "+nome+" e sua idade é: "+idade);
+    }
+}
+
+    nomeComIdade("João");
+    nomeComIdade("João", 12);
+
+
+    function repetirFrase(frase, n=2){
+        for(let x=1; x<=n;x++){
+            console.log(frase + " " + x);
+        }
+    }
+
+    repetirFrase("Testando", 5);
+    repetirFrase("Só duas vezes");
+
+*/
+
+/*
+function retornarNumeroPar(n){
+    if (n%2==0){
+        console.log("n agora é par: "+n);
+    } else {
+        console.log(n);
+        retornarNumeroPar(n-1);
+    }
+}
+
+retornarNumeroPar(33);
+*/
+
+/*
+let n = parseInt(prompt('Digite o número a ser verificado: '));
+verPrimo(n)
+
+
+function verPrimo(numero){
+
+    let contador = 0;
+    let i;
+    
+
+    for(i=2;i<=numero/2;i++){
+        if(numero%i == 0){
+            contador++;
+        }
+        
+    }
+
+    if(contador == 0){
+        console.log('É um número primo');
+    } else {
+        console.log('Não é primo');
+    }
+}
+
+*/
+
+/*
+let dados = [1,3,5,8,23];
+console.log(dados.length);
+
+let nome = "matheus";
+let tam = nome.length;
+console.log(tam);
+*/
+
+/*
+let marca = "Nike";
+
+console.log(typeof marca.toUpperCase());
+console.log(marca.toUpperCase());
+console.log(marca.toLowerCase());
+*/
+
+/*
+let nome = prompt('Digite uma palavra');
+let informa = nome.split('');
+let aux = [];
+let tam = nome.length;
+
+for(i=1;i<=tam;i++){
+    if((informa[tam-i] === 'a') || (informa[tam-i] === 'A')){
+        aux[i-1] = '4';
+    } else if((informa[tam-i] === 'i') || (informa[tam-i] === 'I')){
+        aux[i-1] = '1';
+    } else{
+        aux[i-1] = informa[tam-i];
+    }  
+}
+
+
+console.log(aux);
+*/
+/*
+let frutas=['morango','abacaxi','uva','banana','melancia'];
+frutas.sort();
+
+let coisas=['word','Word','2 Words','wordinho','WOrd'];
+coisas.sort();
+
+console.log(frutas);
+console.log(coisas);
+
+let numeros=[10,65,48,75,12,32,65,49,10,2];
+let verify=1;
+*/
+/*
+numeros.sort(function(a,b){
+    if (a>b) return 1;
+    if (a<b) return -1;
+    return 0;
+
+});
+*/
+/*
+
+let precisa=true;
+let i=0;
+
+while(precisa==true){
+    precisa=false;
+    for (i=0;i<numeros.length-1;i++){
+        if (numeros[i]>numeros[i+1]){
+            aux=numeros[i];
+            numeros[i]=numeros[i+1];
+            numeros[i+1]=aux;
+            precisa=true;
+        }
+    }
+
+}
+    
+console.log(numeros);
+*/
+/*
+numeros=['um',' dois', 'tres', 'quatro'];
+console.log(numeros.reverse());
+
+let meses = ['jan','fev','mar'];
+let mestexto = meses.toString();
+
+console.log(mestexto);
+
+
+mestexto = meses.join(' ');
+
+console.log(mestexto);
+
+console.log([1,2,3,4].fill('batata'));
+console.log([1,2,3,4].fill(0,1));
+console.log([1,2,3,4].fill(0,1,1));
+console.log([1,2,3,4].fill(0,1,2));
+console.log([1,2,3,4].fill(0,1,3));
+*/
+/*
+const reg1=new RegExp('bola');
+console.log(reg1.test('Tem bola!'));
+
+console.log(reg1.test('Não tem'));
+let texto='Tem bola na cesta';
+console.log(reg1.test(texto));
+
+console.log(/TESTE/.test("TTTTESTEEEE"));
+console.log(/TESTE/.test("TTTTESTTE"));
+*/
+
+/*
+console.log(/[12345]/.test('Temos o numero 23'));
+console.log(/[12345]/.test('Temos o numero 6'));
+console.log(/[12345]/.test('Temos o numero 25'));
+console.log(/[0-9]/.test('Temos o numero 60'));
+console.log(/[0-9]/.test('Temos o numero 6159782350'));
+*/
+
+/*
+const pontoRegex = /./;
+console.log(pontoRegex.test('asd'));
+console.log(pontoRegex.test(' '));
+console.log(pontoRegex.test('123'));
+console.log(pontoRegex.test('123asd'));
+*/
+
+/*
+const dRegex = /\d/;
+console.log(dRegex.test('asd'));
+console.log(dRegex.test(' '));
+console.log(dRegex.test('123'));
+console.log(dRegex.test('123asd'));
+*/
+
+/*
+const sRegex = /\s/;
+console.log(sRegex.test('asd'));
+console.log(sRegex.test(' '));
+console.log(sRegex.test('123'));
+console.log(sRegex.test('123asd'));
+*/
+
+/*
+const dRegex2 = /\D/;
+console.log(dRegex2.test('asd'));
+console.log(dRegex2.test(' '));
+console.log(dRegex2.test('123'));
+console.log(dRegex2.test('123asd'));
+*/
+
+/*
+const wRegex = /\w/;
+console.log(wRegex.test('asd'));
+console.log(wRegex.test(' '));
+console.log(wRegex.test('123'));
+console.log(wRegex.test('123asd'));
+*/
+
+/*
+const wRegex2 = /\W/;
+console.log(wRegex2.test('asd'));
+console.log(wRegex2.test(' '));
+console.log(wRegex2.test('123'));
+console.log(wRegex2.test('123asd'));
+*/
+
+/*
+const ano = /\d\d\d\d/;
+console.log(ano.test('05'));
+console.log(ano.test('051'));
+console.log(ano.test('0516'));
+console.log(ano.test('05169'));
+console.log(ano.test('051690'));
+console.log(ano.test('opa'));
+console.log(ano.test('0o1p2a3'));
+console.log(ano.test('opa3456'));
+*/
+
+/*
+const palavras = /\W/;
+console.log(palavras.test('dia'));
+console.log(palavras.test('diario'));
+console.log(palavras.test('dia-01'));
+console.log(palavras.test('dia@1'));
+console.log(palavras.test('s3nh@fort$'));
+*/
+
+/*
+const nume = /[^0123]/;
+console.log(nume.test('1112'));
+console.log(nume.test('03'));
+console.log(nume.test('456'));
+console.log(nume.test('01234'));
+*/
+
+/*
+const notab = /[^ab]/;
+console.log(notab.test('a'));
+console.log(notab.test('Aqui tem a'));
+
+const notatoz = /[^a-z]/;
+console.log(notatoz.test('Jaguarete'));
+console.log(notatoz.test('Aqui tem a'));
+console.log(notatoz.test('456 '));
+console.log(notatoz.test('Jaguarete4'));
+*/
+
+/*
+let repete = /\d+/;
+console.log(repete.test('1'));
+console.log(repete.test('03'));
+console.log(repete.test('456'));
+console.log(repete.test('1234xx'));
+*/
+
+let opcional = /Provas\s?\d?/;
+console.log(opcional.test('Provas'));
+console.log(opcional.test('Provas 1'));
